ce98c0923a1cf4c741e2a928a1afd4e1a8a3b762
Pattarapon Makhirun <pattarapon.it@mail.kmutt.ac.th>
2021-09-09 14:36:29 +0700

    [week5]

---
+++ inclass_files/script.js
@@ -0,0 +1,22 @@
+const questions = [
+  {
+    Q: "1+2=?",
+    A: {
+      a: "3",
+      b: "2",
+      c: "1"
+    },
+    correctA: "a"
+  },
+  {
+    Q: "1-8=?",
+    A: {
+      a: "8",
+      b: "7",
+      c: "6"
+    },
+    correctA: "7"
+  }
+];
+
+console.log(questions);
