a0942be94830cca0c4404a0ceb71a6542cf0d638
Pattarapon Makhirun <pattarapon131@gmail.com>
2021-11-10 21:17:08 +0700

    [commit]

--- exercises/DOM/DOM.js
+++ exercises/DOM/DOM.js
@@ -1,2 +1,18 @@
-const body = document.body
-body.append('Hello')
+const navBar = document.querySelector('#navbar');
+let navElem = document.createElement('nav');
+navElem.setAttribute('style','background-color : #ffffff');
+navElem.innerHTML = (`
+                <ul class="flex">
+                        <li class="mr-6">
+                            <a class="text-blue-500 hover:text-blue-800" href="#">Active</a>
+                         </li>
+                        <li class="mr-6">
+                            <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+                        </li>
+                        <li class="mr-6">
+                            <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+                        </li>
+                        
+                </ul>`);
+
+navBar.appendChild(navElem);
